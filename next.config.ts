787b7cbeec113510b0de2dd2a8ffca015ce9f72d
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2026-04-12 22:31:45 +0100

    config(next): add supabase remote pattern for herald logo

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   redirects: async () => [
     { source: '/docs', destination: '/docs/getting-started', permanent: false },
   ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ucshdejvxzanuxlxrano.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 const withMDX = createMDX();
